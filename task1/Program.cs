62aa161eaced0be3f85126838207f511bf8f132a
John Doe <ola_soldattova@mail.ru>
2023-04-21 23:19:03 +0300

    Семинар 7

---
+++ task1/Program.cs
@@ -0,0 +1,42 @@
+﻿// Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+int ReadInt(string message)
+{
+    System.Console.Write($"{message} >");
+    return Convert.ToInt32(Console.ReadLine());
+}
+int rows = ReadInt("Введите количество строк");
+int colums = ReadInt("Введите количество столбцов");
+double[,] array = new double[rows, colums];
+
+double[,] Generate2DArray(double rows, double solums)
+{
+    Random rnd = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(-99, 99) / 10.0;
+        }
+    }
+    return array;
+}
+
+void Print2DArray(double[,] array)
+{
+   for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            System.Console.Write($"{array[i, j]}\t");
+        } 
+        System.Console.WriteLine();
+    }
+}
+
+double[,] arr = Generate2DArray(rows, colums);
+Print2DArray(arr);
---
+++ task2/Program.cs
@@ -0,0 +1,59 @@
+﻿// Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, 
+// что такого элемента нет.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 1, 7 -> такого числа в массиве нет
+// 1, 1 -> 1
+
+int ReadInt(string message)
+{
+    System.Console.Write($"{message} > ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+int rows = ReadInt("Введите количество строк");
+int colums = ReadInt("Введите количество столбцов");
+int[,] array = new int[rows, colums];
+
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+        array[i, j] = new Random().Next(0,21);
+        }
+    }
+
+   for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            System.Console.Write($"{array[i, j]}\t");
+        } 
+        System.Console.WriteLine();
+    }
+
+Console.Write("Введите индекс строки нужного элемента: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите индекс столбца нужного элемента: ");
+int b = Convert.ToInt32(Console.ReadLine());
+for (int i = 0; i < array.GetLength(0); i++)
+{
+ if (a == i + 1)
+    {
+ for (int j = 0; j < array.GetLength(1); j++)
+        {
+ if (b == j + 1)
+            {
+ Console.WriteLine($"Значение [{a}, {b}] элемента массива: {array[i, j]}");
+            }
+            
+        }
+    }
+}
+if (a > array.GetLength(0) || b > array.GetLength(1))
+{
+Console.WriteLine("Такого числа в массиве нет!");
+}
